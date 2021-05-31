7b0884cb3b88be679754871a7c2e6c393f6c9533
jascha.merle <jascha.merle@epita.fr>
2021-05-31 13:46:52 +0200

    begining of project

---
+++ JDBLite/Communication/Channel.cs
@@ -0,0 +1,16 @@
+using Discord.Commands;
+
+namespace Communication
+{
+    public class Channel
+    {
+        private long _discordId;
+        private string _name;
+        private Category _category;
+
+        public Channel()
+        {
+            
+        }
+    }
+}
---
+++ JDBLite/Communication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Communication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
